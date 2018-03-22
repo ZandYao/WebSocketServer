c85e810fe553eda3110f9e9ad9c416e2bc8e47e8
Andrew Alexander Balogh <andrewalexanderbalogh@gmail.com>
2018-03-22 00:05:37 -0400

    demo ready

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 'use strict';
 
-const app = require('express')();
+const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
 const path = require('path');
 // const url = require('url');
 const uuid = require('uuid');
 const http = require('http');
-//const http = require('http').Server(app);
 const WebSocket = require('ws');
 
+const app = express();
 
+/* Control constants that define the context of the message sent */
 const CONTROL_MESSAGES = Object.freeze({
-    SHUT_DOWN : 'SHUT_DOWN',
-    SET_UUID: 'SET_UUID',
+    RESET_SERVER : 'RESET_SERVER',
     SEND_DATA: 'SEND_DATA',
     BROADCAST: 'BROADCAST',
     INIT_CLIENT: 'INIT_CLIENT',
+    TARGET_CLIENT: 'TARGET_CLIENT',
 });
 
-
-
-// We will have these headers applied to all our responses.
+/* We will have these headers applied to all our http responses. */
 const responseHeaders = function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Allow-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -30,80 +29,123 @@ const responseHeaders = function(req, res, next){
 };
 app.use(responseHeaders);
 
-// create a write stream (in append mode) & setup logging for http requests
+/* We will serve up our demo-web-page too. */
+app.use(express.static('webcontent'));
+
+/* Create a write stream (in append mode) & setup logging for http requests (does not log WebSocket connections. maybe can find a good library to..) */
 const accessLogStream = fs.createWriteStream(path.resolve(`${__dirname}/../access.log`), {flags: 'a'});
 app.use(morgan('combined', {stream: accessLogStream}));
 
-// app.get('/', function(req, res){
-//     res.sendFile( path.resolve(`${__dirname}/../webcontent/index.html`));
-// });
-//
-// http.listen(3000, function(){
-//     console.log('listening on *:3000');
-// });
-
-
+/* We startup our http and WebSocket servers. We also make a Map to easily track the specifics of connected clients */
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server, clientTracking: true });
-const connectedClients = new Map();
+let wss = {};
 
-const broadCastMessage = function(){
-
+/* Broadcast a message to all connected clients */
+const broadCastMessage = function(type, content){
+    const sendMessage = { type, content };
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(sendMessage));
+        }
+    });
 };
 
-wss.on('connection', function connection(ws, req) {
-    // const location = url.parse(req.url, true);
-    const ip = req.connection.remoteAddress;
-    const id = uuid.v4();
+const startWebSocketServer = function() {
+    wss = new WebSocket.Server({server, clientTracking: true});
+    const connectedClients = new Map();
 
-    connectedClients.set(id, { ws, identity: '', status: '', last_msg: '' });
+    /* Each time a client connects, we kick off this handler */
+    wss.on('connection', function connection(ws, req) {
+        // const location = url.parse(req.url, true);
+        const ip = req.connection.remoteAddress;
+        const id = uuid.v4();
+        console.log(`Connected to ${ip}`);
 
-    // Handle incoming messages from Clients
-    ws.on('message', function (msg) {
-        let sendMessage = {};
-        const message = JSON.parse(msg);
-        console.log(`Received data: ${message.content}, Status: ${message.status}`);
+        /* Store a reference to the client by it's ws object */
+        connectedClients.set(ws, {identity: '', status: '', last_msg: '', ip});
 
-        switch(message.type){
-        case CONTROL_MESSAGES.SHUT_DOWN:
-            wss.close();
-            break;
-        case CONTROL_MESSAGES.INIT_CLIENT:
-            connectedClients.get(message.uuid).identity = message.content;
-            connectedClients.get(message.uuid).status = message.status;
-            connectedClients.get(message.uuid).last_msg = message.content;
+        /* Handle incoming messages from Client */
+        ws.on('message', function (msg) {
+            let sendMessage = {};
+            const message = JSON.parse(msg);
+            console.log(`Received data: ${message.content}, Status: ${message.status}`);
 
-            broadCastMessage();
-            break;
-        case CONTROL_MESSAGES.SEND_DATA:    /* falls through */
-        default:
-            connectedClients.get(message.uuid).status = message.status;
-            connectedClients.get(message.uuid).last_msg = message.content;
+            switch (message.type) {
+                case CONTROL_MESSAGES.RESET_SERVER:
+                wss.close();
+                /* Restart the server 5 seconds after closing it */
+                setTimeout(() => startWebSocketServer(), 5000);
+                break;
+            case CONTROL_MESSAGES.INIT_CLIENT:
+                connectedClients.get(ws).identity = message.content;
+                connectedClients.get(ws).status = message.status;
+                connectedClients.get(ws).last_msg = message.content;
 
-            sendMessage = { type: CONTROL_MESSAGES.SEND_DATA, content: `echo something from ${ip}: ${message.content}` };
-            ws.send(JSON.stringify(sendMessage));
-        }
+                /* Broadcast to all clients that a new Client has connected */
+                broadCastMessage(CONTROL_MESSAGES.INIT_CLIENT, [...connectedClients.values()]);
+                break;
+
+            case CONTROL_MESSAGES.TARGET_CLIENT:
+                const target = [...connectedClients.entries()]
+                    .filter((clientEntry) => clientEntry[1].identity === message.target)
+                    .pop();
+                if (target){
+                    sendMessage = {
+                        type: CONTROL_MESSAGES.SEND_DATA,
+                        content: `You got "${message.content}" from ${connectedClients.get(ws).identity}`
+                    };
+                    target[0].send(JSON.stringify(sendMessage));
+                }
+                else{
+                    sendMessage = {
+                        type: CONTROL_MESSAGES.SEND_DATA,
+                        content: `ERROR: "${message.target}" is not connected to the server`
+                    };
+                    ws.send(JSON.stringify(sendMessage));
+                }
+                break;
+
+            case CONTROL_MESSAGES.BROADCAST:
+                broadCastMessage(CONTROL_MESSAGES.BROADCAST, `Broadcast "${message.content}" sent from ${connectedClients.get(ws).identity} at ${ip}`);
+                break;
+            case CONTROL_MESSAGES.SEND_DATA:    /* falls through */
+            default:
+                connectedClients.get(ws).status = message.status;
+                connectedClients.get(ws).last_msg = message.content;
+
+                sendMessage = {
+                    type: CONTROL_MESSAGES.SEND_DATA,
+                    content: `You (${connectedClients.get(ws).identity}) sent "${message.content}" from ${ip}`
+                };
+                ws.send(JSON.stringify(sendMessage));
+            }
+
+        });
+
+        /* Handle erroneous issues */
+        ws.on('error', function (error) {
+            console.log(`Error occurred: ${error}`);
+        });
+
+        /* Handle a client disconnecting */
+        ws.on('close', function (code, reason) {
+            console.log(`Connection closed. Code: ${code}, Reason: ${reason}`);
+            connectedClients.delete(ws);
+
+            /* Broadcast to all clients that a Client has left */
+            broadCastMessage(CONTROL_MESSAGES.INIT_CLIENT, [...connectedClients.values()]);
+        });
+
 
     });
+};
 
-
-    ws.on('error', function(error){
-        console.log(`Error occurred: ${error}`);
-    });
-
-
-    ws.on('close', function(code, reason){
-        console.log(`Connection closed. Code: ${code}, Reason: ${reason}`);
-        connectedClients.delete(id);
-    });
-
-
-    console.log(`Connected to ${ip}`);
-    const initMessage = { type: CONTROL_MESSAGES.SET_UUID, content: id };
-    ws.send(JSON.stringify(initMessage));
-});
-
+/* Start the Server */
+startWebSocketServer();
 
 server.listen(8080, function listening() {
+    const serverParams = wss.address();
     console.log(`Listening on %d`, server.address().port);
+    // console.log(`${wss.url}`);
+    console.log(`${serverParams.address} | ${serverParams.family} | ${serverParams.port}`);
 });
--- webcontent/socketmsg.js
+++ webcontent/socketmsg.js
@@ -1,44 +1,65 @@
+/* Shortcuts to DOM elements */
 const msgType = document.getElementById('msg_type');
 const clientStatus = document.getElementById('client_status');
 const clientIdentity = document.getElementById('client_identity');
 const textMessage = document.getElementById('textmsg');
+const wsClientData = document.getElementById('websocketClientData');
+const wsClientDisplay = document.getElementById('websocketClients');
+const wsTargets = document.getElementById('target_client');
 
 const connectionStatus = document.getElementById('status');
 const errorMsg = document.getElementById('errormsg');
-const uuidDisplay = document.getElementById('uuid');
 const results = document.getElementById('results');
 
 const sendMessage = document.getElementById('send');
 const makeConnection = document.getElementById('connect');
 const closeConnection = document.getElementById('close');
 
+/* Control constants that define the context of the message sent */
 const CONTROL_MESSAGES = Object.freeze({
-    SHUT_DOWN: 'SHUT_DOWN',
+    RESET_SERVER: 'RESET_SERVER',
     SET_UUID: 'SET_UUID',
     SEND_DATA: 'SEND_DATA',
     BROADCAST: 'BROADCAST',
     INIT_CLIENT: 'INIT_CLIENT',
+    TARGET_CLIENT: 'TARGET_CLIENT',
 });
 
 //const wsAddress = 'ws://echo.websocket.org/';
 const wsAddress = 'ws://192.168.1.150:8080/';
-const clientInfo = {};
 let websocket = {};
 clientStatus.value = 'OK';
 clientIdentity.value = `VAC${Math.floor(Math.random() * 10000)}`;
 
-// Handle incoming message from WebSocket Server
+/* Handle incoming message from WebSocket Server */
 const handleIncoming = function(msg){
     const messageData = JSON.parse(msg.data);
 
     switch (messageData.type){
-    case CONTROL_MESSAGES.SET_UUID:
-            uuidDisplay.innerHTML = clientInfo.uuid = messageData.content;
+    case CONTROL_MESSAGES.INIT_CLIENT:
+        console.log(messageData);
 
-            // Now that we have a uuid on the server, we send the WebSocket server some info on ourselves to help id us more.
-            const sendMessage = { type: CONTROL_MESSAGES.INIT_CLIENT, status: clientStatus.value, content: clientIdentity.value, uuid: clientInfo.uuid };
-            websocket.send(JSON.stringify(sendMessage));
-            break;
+        let newTableData = '';
+        let targetOptions = '';
+        messageData.content.forEach((client) => {
+            newTableData = `${newTableData}
+                <tr>
+                    <td>${client.identity}</td>
+                    <td>${client.ip}</td>
+                    <td>${client.status}</td>
+                </tr>`;
+
+
+            targetOptions = `${targetOptions}
+                <option value="${client.identity}">${client.identity}</option>`;
+
+        });
+        wsTargets.disabled = false;
+        wsTargets.innerHTML = targetOptions;
+        wsClientData.innerHTML = newTableData;
+        wsClientDisplay.classList.remove('no-connect');
+        wsTargets.classList.remove('no-connect');
+        break;
     case CONTROL_MESSAGES.SEND_DATA:    /* falls through */
     default:
         results.value = messageData.content;
@@ -46,6 +67,7 @@ const handleIncoming = function(msg){
     }
 };
 
+/* Open a connection to the WebSocket Server */
 makeConnection.addEventListener('click', () => {
     websocket = new WebSocket(wsAddress);
     errorMsg.classList.add('hidden');
@@ -57,7 +79,8 @@ makeConnection.addEventListener('click', () => {
         closeConnection.disabled = false;
         sendMessage.disabled = false;
 
-
+        const message = { type: CONTROL_MESSAGES.INIT_CLIENT, status: clientStatus.value, content: clientIdentity.value };
+        websocket.send(JSON.stringify(message));
     };
 
     websocket.onclose = function(evt){
@@ -66,6 +89,9 @@ makeConnection.addEventListener('click', () => {
         makeConnection.disabled = false;
         closeConnection.disabled = true;
         sendMessage.disabled = true;
+        wsTargets.disabled = true;
+        wsClientDisplay.classList.add('no-connect');
+        wsTargets.classList.add('no-connect');
 
         console.log(`Closed with code: ${evt.code}`);
     };
@@ -77,16 +103,32 @@ makeConnection.addEventListener('click', () => {
     };
 });
 
-
+/* Open a connection to the WebSocket Server */
 closeConnection.addEventListener('click', () => {
     websocket.close(1000, 'Done for the day');
     makeConnection.disabled = false;
     closeConnection.disabled = true;
     sendMessage.disabled = true;
+    wsTargets.disabled = true;
+    wsClientDisplay.classList.add('no-connect');
+    wsTargets.classList.add('no-connect');
 });
 
-
+/* Send a message with a action type based on what was chosen from the selector */
 sendMessage.addEventListener('click', () => {
-    const message = { type: msgType.value, status: clientStatus.value, content: textMessage.value, uuid: clientInfo.uuid };
+    let message = { type: '', status: '', content: ''};
+    switch(msgType.value){
+    case CONTROL_MESSAGES.RESET_SERVER:
+        message = { type: CONTROL_MESSAGES.RESET_SERVER };
+        break;
+    case CONTROL_MESSAGES.BROADCAST:    /* falls through */
+    case CONTROL_MESSAGES.SEND_DATA:
+        message = { type: msgType.value, status: clientStatus.value, content: textMessage.value };
+        break;
+    case CONTROL_MESSAGES.TARGET_CLIENT:
+        message = { type: CONTROL_MESSAGES.TARGET_CLIENT, content: textMessage.value, target: wsTargets.value };
+        break;
+    }
+
     websocket.send(JSON.stringify(message));
 }, false);
